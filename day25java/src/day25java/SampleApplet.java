24b0c7dccf24b719a7c3198a8350135e9d424ca9
ruman <mohammedruman1996@gmail.com>
2022-03-05 00:09:25 +0530

    practiced.

--- day25java/src/day25java/SampleApplet.java
+++ day25java/src/day25java/SampleApplet.java
@@ -8,26 +8,34 @@ import java.awt.event.ActionListener;
 
 public class SampleApplet extends Applet
 {
-   Button b,b1;
-   TextField t;
+   Button b,b1,b2;
+   TextField t,t1;
    public void init()
    {
 	   add(b = new Button("Click"));
 	   add(t = new TextField(15));
 	   add(b1 = new Button("Clear"));
+	   add(t1 = new TextField(15));
+	   add(b2 = new Button("Submit"));
 	   b.addActionListener(new A());
+	   b2.addActionListener(new A());
 	   b1.addActionListener(new ActionListener()
 			   {
 	   public void actionPerformed(ActionEvent e)
 	   {
-		   t.setText(" ");
+		   t.setText("          ");
+		   
+	   }
+	   {
+		   t1.setText("         ");
 	   }
 	   });
 	   }
    class A implements ActionListener { 
 	   public void actionPerformed(ActionEvent e)
 	   {
-		   t.setText("hello");
+		   t.setText("Hello java"); 
+		   t1.setText("welcome");
 	   }
    }
  }
